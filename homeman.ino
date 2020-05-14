2ce8a488448e4808c8243c0bfb561479a7b695a4
Thuy Nguyen <nhuythuy@gmail.com>
2020-05-14 12:51:33 +0200

    Update homeman.ino
    add NTP time check

--- homeman.ino
+++ homeman.ino
@@ -12,6 +12,7 @@
 #include <ESP8266WiFi.h>
 #include "ThingSpeak.h"
 #include "wifi_pw.h"
+#include <WiFiUdp.h>
 
 // replace with your channel's thingspeak API key, 
 //const char* ssid = "matsuya"; //"VNNO";
@@ -23,7 +24,17 @@ unsigned long THING_SPEAK_CHANNEL_NO = 447257;
 String myWriteAPIKey = "59Y4RMCCJVKVWBOQ";
 // to send data, using this get req: https://api.thingspeak.com/update?api_key=QFS00KA70KNC5NX6&field8=6
 
-#define DELAY_LONG 30000    // 30 seconds
+
+unsigned int localPort = 2390;      // local port to listen for UDP packets
+//IPAddress timeServer(129, 6, 15, 28); // time.nist.gov NTP server
+IPAddress timeServerIP; // time.nist.gov NTP server address
+const char* ntpServerName = "time.nist.gov";
+const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
+byte packetBuffer[ NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
+WiFiUDP udp; // A UDP instance to let us send and receive packets over UDP
+
+
+#define DELAY_LONG 10000    // 30 seconds
 #define DELAY_SHORT 3000    // 3 seconds
 
 String myReadAPIKey = "9L9ZWCW1QLN39E09";
@@ -48,8 +59,9 @@ const char* server = "api.thingspeak.com";
 
 DHT dht(PIN_SS_DHT, DHT11,15);
 WiFiClient client;
-   
- 
+
+
+
 void setup() {                
   Serial.begin(115200);
   delay(10);
@@ -77,6 +89,69 @@ void setup() {
 
   Serial.println(WiFi.localIP());
   Serial.println("WiFi connected");
+
+
+  // internet time
+  Serial.println("Starting UDP");
+  udp.begin(localPort);
+  Serial.print("Local port: ");
+  Serial.println(udp.localPort());
+
+  //get a random server from the pool
+  WiFi.hostByName(ntpServerName, timeServerIP);
+
+  sendNTPpacket(timeServerIP); // send an NTP packet to a time server
+  // wait to see if a reply is available
+  delay(1000);
+  
+  int cb = udp.parsePacket();
+  if (!cb) {
+    Serial.println("no packet yet");
+  }
+else {
+    Serial.print("packet received, length=");
+    Serial.println(cb);
+    // We've received a packet, read the data from it
+    udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
+
+    //the timestamp starts at byte 40 of the received packet and is four bytes,
+    // or two words, long. First, esxtract the two words:
+
+    unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
+    unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+    // combine the four bytes (two words) into a long integer
+    // this is NTP time (seconds since Jan 1 1900):
+    unsigned long secsSince1900 = highWord << 16 | lowWord;
+    Serial.print("Seconds since Jan 1 1900 = " );
+    Serial.println(secsSince1900);
+
+    // now convert NTP time into everyday time:
+    Serial.print("Unix time = ");
+    // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
+    const unsigned long seventyYears = 2208988800UL;
+    // subtract seventy years:
+    unsigned long epoch = secsSince1900 - seventyYears;
+    // print Unix time:
+    Serial.println(epoch);
+
+
+    // print the hour, minute and second:
+    Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
+    Serial.print((epoch  % 86400L) / 3600); // print the hour (86400 equals secs per day)
+    Serial.print(':');
+    if ( ((epoch % 3600) / 60) < 10 ) {
+      // In the first 10 minutes of each hour, we'll want a leading '0'
+      Serial.print('0');
+    }
+    Serial.print((epoch  % 3600) / 60); // print the minute (3600 equals secs per minute)
+    Serial.print(':');
+    if ( (epoch % 60) < 10 ) {
+      // In the first 10 seconds of each minute, we'll want a leading '0'
+      Serial.print('0');
+    }
+    Serial.println(epoch % 60); // print the second
+  }
+
 
   pinMode(PIN_WORKING_MODE, INPUT);
 
@@ -166,9 +241,9 @@ void loop() {
 
 void blinkLed()
 {
-  digitalWrite(PIN_LED, true);
-  delay(200);
   digitalWrite(PIN_LED, false);
+  delay(200);
+  digitalWrite(PIN_LED, true);
 }
 bool updateHumidTempe(){
   humidity = dht.readHumidity();
@@ -233,4 +308,29 @@ void updateActuator()
       digitalWrite(PIN_AC_POWER_LOAD, 0 || forceCamPower);
     else
       digitalWrite(PIN_AC_POWER_LOAD, 1);
+}
+
+// send an NTP request to the time server at the given address
+unsigned long sendNTPpacket(IPAddress& address)
+{
+  Serial.println("sending NTP packet...");
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12]  = 49;
+  packetBuffer[13]  = 0x4E;
+  packetBuffer[14]  = 49;
+  packetBuffer[15]  = 52;
+
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:
+  udp.beginPacket(address, 123); //NTP requests are to port 123
+  udp.write(packetBuffer, NTP_PACKET_SIZE);
+  udp.endPacket();
 }
