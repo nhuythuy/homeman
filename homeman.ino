02c420f1f476870abaed28d285db7063bccf43ea
Thuy Nguyen <nhuythuy@gmail.com>
2020-05-14 10:53:52 +0200

    Update homeman.ino
    updated pin

--- homeman.ino
+++ homeman.ino
@@ -14,10 +14,10 @@
 #include "wifi_pw.h"
 
 // replace with your channel's thingspeak API key, 
-const char* ssid = "matsuya"; //"VNNO";
-const char* password = ""; // ""; // WIFI_PW;
-//const char* ssid = "DNVGuest";
-//const char* password = "";
+//const char* ssid = "matsuya"; //"VNNO";
+//const char* password = "kamikaze123"; // "Planetvegen18A"; // WIFI_PW;
+const char* ssid = "DNVGuest";
+const char* password = WIFI_PW;
 
 unsigned long THING_SPEAK_CHANNEL_NO = 447257;
 String myWriteAPIKey = "59Y4RMCCJVKVWBOQ";
@@ -26,28 +26,27 @@ String myWriteAPIKey = "59Y4RMCCJVKVWBOQ";
 #define DELAY_LONG 30000    // 30 seconds
 #define DELAY_SHORT 3000    // 3 seconds
 
-int pinLed = D6;
 String myReadAPIKey = "9L9ZWCW1QLN39E09";
-
 const char* server = "api.thingspeak.com";
-#define DHTPIN D2 // what pin we're connected to
-#define DHTTYPE DHT11   // DHT 11
 
-int pinTempSensor = A0;
+#define PIN_SS_ANALOG       A0
 
+#define PIN_LED             D4 // same as built in LED GPIO2
 #define PIN_WORKING_MODE    D0
-#define PIN_SS_DOOR_MAIN    D3
-#define PIN_SS_DOOR_BACK    D4
+#define PIN_SS_DHT          D1 // DHT sensor pin
+#define PIN_SS_DOOR_MAIN    D2
+#define PIN_SS_DOOR_BACK    D3
+
 #define PIN_SS_DOOR_DOWN_BASEMENT   D5
-#define PIN_SS_WATER_LEAK_BASEMENT  D6
+#define PIN_SS_WATER_SMOKE_BASEMENT D6 // smoke, water leak
 
 #define PIN_AC_POWER_LOAD   D8
 #define PIN_AC_POWER_CAM    D9
 #define PIN_AC_BUZZER       D10
 #define FIELD_ID_POWER_CAM  8
-#define FIELD_ID_POWER_LOAD  9
+#define FIELD_ID_POWER_LOAD 9
 
-DHT dht(DHTPIN, DHT11,15);
+DHT dht(PIN_SS_DHT, DHT11,15);
 WiFiClient client;
    
  
@@ -82,10 +81,11 @@ void setup() {
   pinMode(PIN_WORKING_MODE, INPUT);
 
   pinMode(PIN_SS_DOOR_MAIN, INPUT);
-  pinMode(PIN_SS_DOOR_BACK, INPUT);
+//  pinMode(PIN_SS_DOOR_BACK, INPUT);
   pinMode(PIN_SS_DOOR_DOWN_BASEMENT, INPUT);
-  pinMode(PIN_SS_WATER_LEAK_BASEMENT, INPUT);
+  pinMode(PIN_SS_WATER_SMOKE_BASEMENT, INPUT);
 
+  pinMode(PIN_LED, OUTPUT);
   pinMode(PIN_AC_POWER_LOAD, OUTPUT);
   pinMode(PIN_AC_POWER_CAM, OUTPUT);
 
@@ -95,8 +95,8 @@ void setup() {
 
 long delayMs = DELAY_LONG;
 // sensors
-float humidity = 0;
-float temp = 0;
+float humidity = 0.0;
+float temp = 0.0;
 
 int ssSmokeDetectors = 0;
 int ssDoorDetectors = 0;
@@ -112,7 +112,14 @@ float camPower = 0;
 float loadPower = 0;
 
 void loop() {
-   
+  bool ret = updateHumidTempe();
+  if(!ret){
+    humidity = -100.0;
+    temp = -100.0;
+  }
+
+  blinkLed();
+
   updateSensors();
   updateWorkingMode();
   updateActuator();
@@ -157,14 +164,22 @@ void loop() {
   delay(delayMs);  
 }
 
-void updateTemperature(){
+void blinkLed()
+{
+  digitalWrite(PIN_LED, true);
+  delay(200);
+  digitalWrite(PIN_LED, false);
+}
+bool updateHumidTempe(){
   humidity = dht.readHumidity();
   temp = dht.readTemperature();
   if (isnan(humidity) || isnan(temp)) {
     Serial.println("Failed to read from DHT sensor!");
     delay(delayMs);
-    return;
+    return false;
   }
+
+  return true;
 }
 
 void updateWorkingMode(){
@@ -181,10 +196,10 @@ void updateSensors(){
 //  bool ssSmokeFirstFloor = digitalRead(PIN_SS_SMOKE_1_FLOOR);
 
   bool ssDoorMain = digitalRead(PIN_SS_DOOR_MAIN);
-  bool ssDoorBack = digitalRead(PIN_SS_DOOR_BACK);
+  bool ssDoorBack = 0;//digitalRead(PIN_SS_DOOR_BACK);
   bool ssDoorDownBasement = digitalRead(PIN_SS_DOOR_DOWN_BASEMENT);
 
-  bool ssWaterLeak = digitalRead(PIN_SS_WATER_LEAK_BASEMENT);
+  bool ssWaterLeak = digitalRead(PIN_SS_WATER_SMOKE_BASEMENT);
 
 //  ssSmokeDetectors = (ssSmokeKichen << 3) | (ssSmokeLivingRoom << 2) | (ssSmokeFirstFloor << 1);
   ssDoorDetectors = (ssDoorDownBasement << 2) | (ssDoorBack << 1) | (ssDoorMain << 0);
