955eb9f479f2426cf023a48e47fb444ca01088a7
Thuy Nguyen <nhuythuy@gmail.com>
2020-07-17 23:37:45 +0200

    changed pin

--- pin_define.h
+++ pin_define.h
@@ -3,7 +3,7 @@
 //#define PIN_SS_ANALOG               A0
 
 #define PIN_SS_DHT                  D5 // DHT sensor pin
-#define PIN_SS_DOOR_MAIN            D6 // main door sensor
+#define PIN_SS_DOOR_MAIN            D10 // main door sensor
 
 #define PIN_SS_DOOR_BASEMENT        D3 // No. 1, door to go down to basement and door to renting area
 #define PIN_SS_ENTRANCE_MOTION      D2 // No. 2, motion sensor for entrance
@@ -14,7 +14,7 @@
 
 // outputs
 #define PIN_LED                     D4 // D4: same as built in LED GPIO2
-#define PIN_TONE_MELODY             D1
+#define PIN_TONE_MELODY             D6
 
 #define PIN_AC_POWER_LED_ENTRANCE   D0 // No. 1, power for entrance led
 //#define PIN_AC_POWER_CAMERA         D1 // No. 2, camera power
