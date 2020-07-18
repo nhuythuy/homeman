f790b901a070c9bb7e3fe2384fc88629bec8c153
Thuy Nguyen <nhuythuy@gmail.com>
2020-07-18 23:17:22 +0200

    updated pin as recommendation, need to fix D3 later
    ref.: https://randomnerdtutorials.com/esp8266-pinout-reference-gpios/

--- pin_define.h
+++ pin_define.h
@@ -1,20 +1,20 @@
 #include <ESP8266WiFi.h>
-// inputs
+// inputs D1, D2, D5, D6, D7
 //#define PIN_SS_ANALOG               A0
 
 #define PIN_SS_DHT                  D5 // DHT sensor pin
 #define PIN_SS_DOOR_MAIN            D10 // main door sensor
 
-#define PIN_SS_DOOR_BASEMENT        D3 // No. 1, door to go down to basement and door to renting area
+#define PIN_SS_DOOR_BASEMENT        D6 // No. 1, door to go down to basement and door to renting area
 #define PIN_SS_ENTRANCE_MOTION      D2 // No. 2, motion sensor for entrance
 
 #define PIN_LIGHT_BASEMENT          D7
-#define PIN_SS_WATER_SMOKE_BASEMENT D8 // smoke, water leak
+#define PIN_SS_WATER_SMOKE_BASEMENT D3 // smoke, water leak
 
 
 // outputs
 #define PIN_LED                     D4 // D4: same as built in LED GPIO2
-#define PIN_TONE_MELODY             D6
+#define PIN_TONE_MELODY             D8
 
 #define PIN_AC_POWER_LED_ENTRANCE   D0 // No. 1, power for entrance led
 //#define PIN_AC_POWER_CAMERA         D1 // No. 2, camera power
