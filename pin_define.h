390681780716ac14967d31df635de28c10bcae20
Thuy Nguyen <nhuythuy@gmail.com>
2021-03-02 21:10:46 +0100

    removed sharing with LED pin since it is noisy

--- pin_define.h
+++ pin_define.h
@@ -20,8 +20,8 @@
 
 
 // outputs
-#define PIN_LED                     2  // D4: same as built in LED GPIO2
-#define PIN_AC_TONE_MELODY          2  // same as LED pin to save pin, if ok?
+#define PIN_LED                     2   // same as built in LED GPIO2
+#define PIN_AC_TONE_MELODY          4
 
 #define PIN_AC_POWER_LED_ENTRANCE   21 // No. 1, power for entrance led
 //#define PIN_AC_POWER_RADIO          18 // OK as output, not possible to connect to OPTO input: connected to FLASH button, boot fails if pulled LOW
