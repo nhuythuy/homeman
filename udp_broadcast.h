566da511986ea8913980da91e904af9d85dc2aad
Thuy Nguyen <nhuythuy@gmail.com>
2021-03-22 19:21:31 +0100

    fixing sending json string to UDP

--- udp_broadcast.h
+++ udp_broadcast.h
@@ -13,45 +13,39 @@ WiFiUDP udp;
 #define BROADCAST_PORT  45678
 
 #define BUFFER_LENGTH   200
-
 byte buffJson[BUFFER_LENGTH];
-String sJson;
 
 
-void buildMessage(unsigned long rt){
+int buildMessage(unsigned long rt){
   // https://arduinojson.org/v6/example/
 
   DynamicJsonDocument doc(256);
   doc["node"] = "homeman";
-  doc["runtime"] = String(rt);
-//  doc["battvolt"] = String(ssBatteryVolt, 2);
-//  doc["temp"] = String(bmTemp, 2);
-//  doc["humidity"] = String(bmHumidity, 2);
-//  doc["ssDoorMain"] = ssDoorMain;
-//  doc["doorMainOpenedMinutes"] = doorMainOpenedMinutes;
-//  doc["ssDoorToBasement"] = ssDoorToBasement;
-//  doc["doorToBasementOpenedMinutes"] = doorToBasementOpenedMinutes;
-//  doc["ssDoorBasement"] = ssDoorBasement;
-//  doc["doorBasementOpenedMinutes"] = doorBasementOpenedMinutes;
-//  doc["ssLightBasementOn"] = ssLightBasementOn;
-//  doc["ssEntranceMotion"] = ssEntranceMotion;
-//  doc["entranceMotionSeconds"] = entranceMotionSeconds;
-//  doc["ssWaterLeak"] = ssWaterLeak;
+  doc["runtime"] = rt;
+  doc["battvolt"] = String(ssBatteryVolt, 2);
+  doc["temp"] = String(bmTemp, 2);
+  doc["humidity"] = String(bmHumidity, 2);
+  doc["ssDoorMain"] = ssDoorMain;
+  doc["doorMainOpenedMinutes"] = doorMainOpenedMinutes;
+  doc["ssDoorToBasement"] = ssDoorToBasement;
+  doc["doorToBasementOpenedMinutes"] = doorToBasementOpenedMinutes;
+  doc["ssDoorBasement"] = ssDoorBasement;
+  doc["doorBasementOpenedMinutes"] = doorBasementOpenedMinutes;
+  doc["ssLightBasementOn"] = ssLightBasementOn;
+  doc["ssEntranceMotion"] = ssEntranceMotion;
+  doc["entranceMotionSeconds"] = entranceMotionSeconds;
+  doc["ssWaterLeak"] = ssWaterLeak;
 //  doc["acActuators"] = acActuators;
 
-  serializeJson(doc, sJson);
+  memset(buffJson, 0x00, BUFFER_LENGTH);
+  return serializeJson(doc, buffJson, BUFFER_LENGTH);
 }
 
 void sendBroadcast(unsigned long rt){
-  buildMessage(rt);
-  int len = sJson.length();
-  memset(buffJson, 0x00, sizeof(buffJson));
-
-  byte plain[len];
-  sJson.getBytes(plain, sJson.length());
+  int len = buildMessage(rt);
 
   udp.beginPacket(BROADCAST_IP, BROADCAST_PORT);
-  udp.write(plain, len + 1);
+  udp.write(buffJson, len);
   udp.endPacket();
 
   Serial.println("Broadcast sent: " + String(len));
